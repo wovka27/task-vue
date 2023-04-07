7ce34d0a6c3aa1d25fdd3af4b8200185eb325fd2
Vladimir Demyanchuk <wovka.home@gmail.com>
2023-04-07 19:24:24 +0500

    добавил анимацию

--- src/components/TaskComponent.vue
+++ src/components/TaskComponent.vue
@@ -1,22 +1,35 @@
 <script lang="ts" setup>
-import { ref, watch } from 'vue'
+import { computed, onMounted, ref, watch } from 'vue'
 import TaskList from '@/components/TaskList.vue'
 import TaskPreview from '@/components/TaskPreview.vue'
 
 const activeTab = ref<string>('0')
+const contentRef = ref()
+const contentTransformValue = computed(() =>
+  activeTab.value === '0' ? 0 : -contentRef.value.children[0].children[0].clientWidth
+)
 </script>
 
 <template>
   <div class="TaskComponent">
-    <div class='TaskComponent__section'>
-      <el-tabs class='TaskComponent__tabs' v-model="activeTab">
+    <div class="TaskComponent__section">
+      <el-tabs class="TaskComponent__tabs" v-model="activeTab">
         <el-tab-pane label="Список" />
         <el-tab-pane label="Результат" />
       </el-tabs>
     </div>
-    <div class='TaskComponent__section'>
-      <TaskList v-if="activeTab === '0'" />
-      <TaskPreview v-if="activeTab === '1'" />
+    <div ref="contentRef" class="TaskComponent__section">
+      <div
+        class="TaskComponent__section-inner-wrapper"
+        :style="`transform: translateX(${contentTransformValue === 0 ? '0' : contentTransformValue + 'px'})`"
+      >
+        <div class="TaskComponent__content">
+          <TaskList />
+        </div>
+        <div class="TaskComponent__content">
+          <TaskPreview />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -30,12 +43,24 @@ const activeTab = ref<string>('0')
   display: grid;
   grid-template-columns: 1fr;
   grid-template-rows: repeat(9, 1fr);
+  &__content {
+    flex-shrink: 0;
+    width: 100%;
+  }
   &__section {
     position: relative;
+    &-inner-wrapper {
+      width: 100%;
+      height: 100%;
+      display: flex;
+      flex-wrap: nowrap;
+      transition: all 0.3s ease-in-out;
+    }
     &:first-child {
       grid-row: span 1;
     }
     &:last-child {
+      overflow: hidden;
       grid-row: span 8;
     }
   }
